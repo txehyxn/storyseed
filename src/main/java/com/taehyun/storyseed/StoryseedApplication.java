477f37d79380a924bd1326d5b9c1eb3f6304b096
Taehyun Kim <tae6241@naver.com>
2026-07-22 19:21:34 +0900

    chore: StorySeed 프로젝트 초기 설정 - Spring Boot 프로젝트 생성 - Gradle Wrapper 및 기본 설정 추가 - MySQL 데이터베이스 연결 설정 - JPA 및 Spring Security 의존성 구성 - Thymeleaf 및 Validation 의존성 추가 - 환경변수를 통한 DB 인증정보 설정 - .gitignore 설정

---
+++ src/main/java/com/taehyun/storyseed/StoryseedApplication.java
@@ -0,0 +1,13 @@
+package com.taehyun.storyseed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoryseedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoryseedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/taehyun/storyseed/StoryseedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taehyun.storyseed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoryseedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
